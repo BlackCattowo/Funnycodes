53b9f1a6ae024e00301bb09af1471328bef24bd5
Rafael Henrique Martinez <128187479+BlackCattowo@users.noreply.github.com>
2024-12-14 12:36:14 -0300

    primeiro código

---
+++ advinha.py
@@ -0,0 +1,19 @@
+print("escoha um número de 1 a 10")
+X = input("Digite seu número: ")
+i = 0
+while i < 8000000:
+    i = i + 1
+i = 0
+print("Calculando probabilidades...")
+while i < 80000000:
+    i = i + 1
+i = 0
+print("Lendo mentes...")
+while i < 16000000:
+    i = i + 1
+i = 0
+print("Advinhando o resultado...")
+while i < 8000000:
+    i = i + 1
+i = 0
+print("Seu numero é:", X)
